fda130bb6ad2be2ac3bcc0cba9b94cda24d4fd1c
Michael Austin Burch <86203662+BirdieTX@users.noreply.github.com>
2025-02-14 20:43:55 -0600

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-sudo cp -r dnf.conf /etc/dnf
 cp -r fastfetch /home/birdie/.config
 cp -r fish /home/birdie/.config
 cp -r ghostty /home/birdie/.config
 cp -r user-dirs.dirs /home/birdie/.config
-./remove-packages.sh
-./install-packages.sh
-./install-flatpaks.sh
-cd /home/birdie/.local/share
-git clone https://github.com/svenstaro/genact.git

aff4016181f78778e239c99079dacc962dba3352
BirdieTX <86203662+BirdieTX@users.noreply.github.com>
2025-02-22 12:41:43 -0600

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -61,11 +61,14 @@ sudo dnf copr enable pgdev/ghostty -y
 sudo dnf copr enable tomaszgasior/mushrooms -y
 sudo dnf copr enable zeno/scrcpy -y
 
+echo "Enabling RPM Fusion repositories ..."
+dnf install -y \
+    "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+    "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+
 echo "Installing system packages ..."
 dnf update --refresh -y
 dnf install -y \
-    "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
-    "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" \
     bibata-cursor-themes \
     bat \
     cargo \
@@ -92,65 +95,145 @@ dnf install -y \
     steam-devices \
     terminus-fonts
 
-echo "Cloning repositories as the user ..."
-sudo -u "$SUDO_USER" git clone https://github.com/svenstaro/genact.git "$USER_HOME/.local/share/genact"
-
-echo "Installing Flatpaks..."
+echo "Checking for flatpak updates ..."
 sudo -u "$SUDO_USER" flatpak update -y
-sudo -u "$SUDO_USER" flatpak install -y \
-    org.audacityteam.Audacity \
-    com.usebottles.bottles \
-    com.brave.Browser \
+
+echo "Installing GNOME flatpaks from Flathub: ..."
+echo "  Disk Usage Analyzer"
+echo "  Characters"
+echo "  Connections"
+echo "  Decibels"
+echo "  Document Viewer"
+echo "  Extensions"
+echo "  Logs"
+echo "  Image Viewer"
+echo "  Rhythmbox"
+echo "  Videos"
+
+sudo -u "$SUDO_USER" flatpak install flathub -y \
+    org.gnome.baobab \
     org.gnome.Characters \
     org.gnome.Connections \
-    com.bitwarden.desktop \
-    ca.desrt.dconf-editor \
     org.gnome.Decibels \
-    io.github.freedoom.Phase1 \
-    io.github.endless_sky.endless_sky \
     org.gnome.Evince \
     org.gnome.Extensions \
-    com.github.tchx84.Flatseal \
-    com.geeks3d.furmark \
-    io.github.realmazharhussain.GdmSettings \
-    org.gimp.GIMP \
-    io.github.shiftey.Desktop \
+    org.gnome.Logs \
+    org.gnome.Loupe \
+    org.gnome.Rhythmbox3 \
+    org.gnome.Totem
+
+echo "Installing KDE flatpaks from Flathub: ..."
+echo "  KColorChooser"
+echo "  Kdenlive"
+echo "  KolourPaint"
+echo "  Krita"
+echo "  KStars"
+
+sudo -u "$SUDO_USER" flatpak install flathub -y \
     org.kde.kcolorchooser \
     org.kde.kdenlive \
     org.kde.kolourpaint \
     org.kde.krita \
-    org.kde.kstars \
-    org.gnome.Logs \
-    org.gnome.Loupe \
-    net.lutris.Lutris \
-    org.fedoraproject.MediaWriter \
-    com.obsproject.Studio \
-    md.obsidian.Obsidian \
-    org.onlyoffice.desktopeditors \
-    org.openttd.OpenTTD \
-    com.pokemmo.PokeMMO \
-    me.proton.Mail \
-    com.protonvpn.www \
-    net.davidotek.pupgui2 \
-    com.github.Matoking.protontricks \
-    io.github.Qalculate \
-    org.qbittorrent.qBittorrent \
-    org.gnome.Rhythmbox3 \
-    com.jetbrains.Rider \
-    net.runelite.RuneLite \
-    org.signal.Signal \
+    org.kde.kstars
+
+echo "Installing system utilities from Flathub: ..."
+echo "  Dconf Editor"
+echo "  Flatseal"
+echo "  Solaar"
+echo "  GDM Settings"
+echo "  Fedora Media Writer"
+echo "  qbitTorrent"
+
+sudo -u "$SUDO_USER" flatpak install flathub -y \
+    ca.desrt.dconf-editor \
+    com.github.tchx84.Flatseal \
     io.github.pwr_solaar.solaar \
+    io.github.realmazharhussain.GdmSettings \
+    org.fedoraproject.MediaWriter \
+    org.qbittorrent.qBittorrent
+
+echo "Installing developments tools from Flathub: ..."
+echo "  Rider"
+echo "  VSCodium"
+echo "  GitHub Desktop"
+
+sudo -u "$SUDO_USER" flatpak install flathub -y \
+    com.jetbrains.Rider \
+    com.vscodium.codium \
+    io.github.shiftey.Desktop
+
+echo "Installing games from Flathub: ..."
+echo "  PokeMMO"
+echo "  Endless Sky"
+echo "  Freedom: Phase 1"
+echo "  Runelite"
+echo "  OpenTTD"
+
+sudo -u "$SUDO_USER" flatpak install flathub -y \
+    com.pokemmo.PokeMMO \
+    io.github.endless_sky.endless_sky \
+    io.github.freedoom.Phase1 \
+    net.runelite.RuneLite \
+    org.openttd.OpenTTD \
+
+echo "Installing creator utilities from Flathub: ..."
+echo "  OBS Studio"
+echo "  ytDownloader"
+echo "  Audacity"
+echo "  GNU Image Manipulation Program"
+
+sudo -u "$SUDO_USER" flatpak install flathub -y \
+    com.obsproject.Studio \
+    io.github.aandrew_me.ytdn \
+    org.audacityteam.Audacity \
+    org.gimp.GIMP
+
+echo "Installing Steam, Launchers and Proton utilities from Flathub: ..."
+echo "  Protontricks"
+echo "  Bottles"
+echo "  Steam"
+echo "  Steam Tinker Launch"
+echo "  ProtonPlus"
+echo "  Lutris"
+
+sudo -u "$SUDO_USER" flatpak install flathub -y \
+    com.github.Matoking.protontricks \
+    com.usebottles.bottles \
     com.valvesoftware.Steam \
     com.valvesoftware.Steam.Utility.steamtinkerlaunch \
-    org.gnome.Totem \
-    com.vscodium.codium \
-    io.github.aandrew_me.ytdn
+    com.vysp3r.ProtonPlus \
+    net.lutris.Lutris
+
+echo "Installing all other flatpaks from Flathub: ..."
+echo "Bitwarden"
+echo "Brave"
+echo "FurMark"
+echo "Proton VPN"
+echo "Obsidian"
+echo "Proton Mail"
+echo "ONLYOFFICE Desktop Editors"
+echo "Signal"
+
+sudo -u "$SUDO_USER" flatpak install flathub -y \
+    com.bitwarden.desktop \
+    com.brave.Browser \
+    com.geeks3d.furmark \
+    com.protonvpn.www \
+    md.obsidian.Obsidian \
+    me.proton.Mail \
+    org.onlyoffice.desktopeditors \
+    org.signal.Signal
+
+echo "All flatpaks installed ..."
 echo "Applying grub theme ..."
+
 cd "$USER_HOME/Downloads/darkmatter-grub-theme"
 sudo python3 darkmatter-theme.py --install
-"Replacing system configuration files ..."
-cd "$USER_HOME/Downloads/fedora
-cp -r grub /etc/default/grub
-cp -r config /etx/selinux/config
+
+echo "Replacing system configuration files ..."
+
+cd "$USER_HOME/Downloads/fedora"
+cp -r grub /etc/default
+cp -r config /etc/selinux
 
 echo "Setup complete!"

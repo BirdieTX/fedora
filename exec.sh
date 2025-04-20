98963651ce48ec385b0df566bacc47865ee8f845
Michael Austin Burch <86203662+BirdieTX@users.noreply.github.com>
2025-04-20 12:35:44 -0500

    Commit

--- exec.sh
+++ exec.sh
@@ -13,7 +13,7 @@ cp -r config /etc/selinux
 
 echo "Copying user configuration files ..."
 sudo -u "$SUDO_USER" cp -r fastfetch "$USER_HOME/.config"
-sudo -u "$SUDO_USER" cp -r kitty "$USER_HOME/.config"
+sudo -u "$SUDO_USER" cp -r ghostty "$USER_HOME/.config"
 sudo -u "$SUDO_USER" cp -r mc "$USER_HOME/.config"
 sudo -u "$SUDO_USER" cp -r user-dirs.dirs "$USER_HOME/.config"
 
@@ -38,16 +38,14 @@ echo "1 ..."
 sleep 1
 dnf remove -y \
     abrt \
-    cosmic-edit \
-    cosmic-store \
     firefox \
     gnome-calendar \
     gnome-connections \
     gnome-contacts \
     gnome-maps \
+    gnome-text-editor \
     gnome-tour \
     gnome-weather \
-    okular \
     ptyxis \
     rhythmbox \
     snapshot \
@@ -56,14 +54,10 @@ dnf autoremove -y
 
 echo "Enabling additional repositories ..."
 dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-dnf copr enable herzen/davinci-helper -y
-dnf copr enable kylegospo/grub-btrfs -y
-dnf copr enable tomaszgasior/mushrooms -y
-rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
-printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h\n" | sudo tee -a /etc/yum.repos.d/vscodium.repo
 dnf install -y \
     "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
-    "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+    "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" \
+    --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 
 echo "System update in 3 ..."
 sleep 1
@@ -83,15 +77,16 @@ dnf install --allowerasing -y \
     antimicrox \
     audacity \
     bat \
+    bibata-cursor-theme \
     brave-browser \
     btop \
     cargo \
     cmatrix \
     codium \
-    davinci-helper \
     decibels \
     dconf-editor \
     discord \
+    eza \
     fastfetch \
     ffmpeg \
     flatseal \
@@ -102,7 +97,6 @@ dnf install --allowerasing -y \
     gnome-tweaks \
     google-arimo-fonts \
     google-noto-fonts-all \
-    grub-btrfs-timeshift \
     gstreamer1-plugins-bad-freeworld \
     gstreamer-plugins-espeak \
     gstreamer1-plugin-openh264 \
@@ -127,7 +121,7 @@ dnf install --allowerasing -y \
     mesa-vdpau-drivers-freeworld \
     mc \
     mozilla-openh264 \
-    nautilus-admin \
+    obs-studio \
     papirus-icon-theme \
     pipewire-codec-aptx \
     protontricks \
@@ -156,13 +150,14 @@ sudo -u "$SUDO_USER" flatpak install flathub -y \
     com.bitwarden.desktop \
     com.geeks3d.furmark \
     com.jetbrains.Rider \
-    com.obsproject.Studio \
     com.pokemmo.PokeMMO \
     com.protonvpn.www \
     com.usebottles.bottles \
     com.vysp3r.ProtonPlus \
     io.github.aandrew_me.ytdn \
-    io.github.freedoom.Phase1 \
+    io.github.freedoom.Phase1
+
+sudo -u "$SUDO_USER" flatpak install flathub -y \
     io.github.endless_sky.endless_sky \
     io.github.realmazharhussain.GdmSettings \
     io.github.shiftey.Desktop \
@@ -172,14 +167,13 @@ sudo -u "$SUDO_USER" flatpak install flathub -y \
     net.runelite.RuneLite \
     org.openttd.OpenTTD \
     org.signal.Signal
+
 echo "All flatpaks installed ..."
 echo "Updating bootloader  ..."
 cd "$USER_HOME/Downloads/fedora"
 cp -r grub /etc/default
+grub2-mkfont -s 25 -o /boot/grub2/fonts/JetBrainsBold.pf2 /usr/share/fonts/jetbrains-mono-nl-fonts/JetBrainsMonoNL-Bold.ttf
 grub2-mkconfig -o /etc/grub2.cfg
-
-echo "Setting up greeter ..."
-systemctl disable cosmic-greeter
-systemctl enable gdm
 fastfetch -c examples/10
 echo "Setup complete!"
+fastfetch -c examples/10

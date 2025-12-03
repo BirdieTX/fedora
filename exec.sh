5440cb51aa38ab94b7045b04fbe4a60e48dcd91c
Michael Austin Burch <86203662+BirdieTX@users.noreply.github.com>
2025-12-03 09:25:04 -0600

    Commit

--- exec.sh
+++ exec.sh
@@ -36,7 +36,8 @@ dnf5 install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/te
 dnf5 install -y \
     "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
     "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
-dnf5 install --allowerasing -y \
+dnf5 install --allowerasing \
+    adwaita-gtk2-theme \
     alacritty \
     antimicrox \
     audacity-freeworld \
@@ -53,7 +54,9 @@ dnf5 install --allowerasing -y \
     code \
     d-spy \
     decibels \
+    desktop-backgrounds-compat \
     dconf-editor \
+    dhcp-client \
     discord \
     dolphin-emu \
     elisa-player \
@@ -89,11 +92,22 @@ dnf5 install --allowerasing -y \
     google-roboto-mono-fonts \
     google-roboto-slab-fonts \
     google-rubik-fonts \
+    greybird-dark-theme \
+    greybird-light-theme \
+    greybird-xfce4-notifyd-theme \
+    greybird-xfwm4-theme \
     gstreamer1-plugins-bad-freeworld \
     gstreamer-plugins-espeak \
+    gtk-xfce-engine \
+    gtk3-immodule-xim \
     hardinfo2 \
     htop \
+    ibus-table-chinese-cangjie \
     inotify-tools \
+    im-chooser \
+    im-chooser-xfce \
+    imsettings \
+    imsettings-gsettings \
     jetbrains-mono-fonts-all \
     jetbrainsmono-nerd-fonts \
     kdenlive \
@@ -120,13 +134,23 @@ dnf5 install --allowerasing -y \
     mesa-vulkan-drivers-freeworld \
     mc \
     minecraft-launcher \
+    minicom \
+    mint-y-theme \
     mission-center \
     mozilla-openh264 \
     nautilus-gsconnect \
     nautilus-open-any-terminal \
+    network-manager-applet \
+    NetworkManager-fortisslvpn-gnome \
+    NetworkManager-iodine-gnome \
+    NetworkManager-l2tp-gnome \
+    NetworkManager-libreswan-gnome \
+    NetworkManager-sstp-gnome \
+    NetworkManager-strongswan-gnome \
     nwg-look \
     obs-studio \
     openrgb \
+    openssh-askpass \
     openttd \
     papirus-icon-theme \
     pavucontrol \
@@ -142,23 +166,57 @@ dnf5 install --allowerasing -y \
     radeontop \
     remmina \
     scummvm \
+    setroubleshoot \
     steam \
     snapper \
     sysprof \
+    system-config-language \
     terminus-fonts \
     terminus-fonts-console \
+    Thunar \
+    thunar-archive-plugin \
+    thunar-media-tags-plugin \
+    thunar-volman \
+    tumbler \
     vim-default-editor \
     virt-manager \
     vlc \
     vlc-plugins-freeworld \
     waycheck \
+    xfce4-about \
+    xfce4-appfinder \
+    xfce4-datetime-plugin \
+    xfce4-panel \
+    xfce4-panel-profiles \
+    xfce4-places-plugin \
+    xfce4-power-manager \
+    xfce4-pulseaudio-plugin \
+    xfce4-screensaver \
+    xfce4-screenshooter-plugin \
+    xfce4-session \
+    xfce4-settings \
+    xfce4-taskmanager \
+    xfce4-terminal \
+    xfdesktop \
+    xfwm4 \
+    xfwm4-themes \
+    xorg-x11-drv-amdgpu \
+    xorg-x11-drv-ati \
+    xorg-x11-drv-evdev \
+    xorg-x11-drv-intel \
+    xorg-x11-drv-libinput \
+    xorg-x11-drv-nouveau \
+    xorg-x11-drv-openchrome \
+    xorg-x11-drv-qxl \
+    xorg-x11-drv-vmware \
+    xorg-x11-drv-wacom \
+    xorg-x11-server-Xorg \
+    xorg-x11-xauth \
+    xorg-x11-xinit \
     zed
-dnf5 group install -y xfce-desktop
 dnf5 swap mesa-va-drivers mesa-va-drivers-freeworld -y
 dnf5 swap mesa-vulkan-drivers mesa-vulkan-drivers-freeworld -y
 dnf5 remove -y \
-    blueman \
-    dnfdragora \
     firefox \
     gnome-boxes \
     gnome-connections \

819b060741e3eb21c6c44e13f3b9c27e0b1a3b0e
Michael Austin Burch <86203662+BirdieTX@users.noreply.github.com>
2026-07-26 12:51:14 -0500

    Commit

---
+++ exec.sh
@@ -0,0 +1,222 @@
+#!/bin/bash
+
+set -e
+
+END='\033[0m\n'
+RED='\033[0;31m'
+GRN='\033[0;32m'
+CYN='\033[0;36m'
+
+if [ "$EUID" -ne 0 ]; then
+    printf $RED"Please run as root using sudo!"$END
+    exit 1
+fi
+
+USER_HOME=$(eval printf ~$SUDO_USER)
+
+cp -r etc /
+cp -r usr /
+sudo -u "$SUDO_USER" cp -r .bashrc "$USER_HOME"
+sudo -u "$SUDO_USER" cp -r .config "$USER_HOME"
+sudo -u "$SUDO_USER" cp -r .local "$USER_HOME"
+sudo -u "$SUDO_USER" cp -r .zshrc "$USER_HOME"
+sudo -u "$SUDO_USER" cp -r Pictures "$USER_HOME"
+plymouth-set-default-theme -R fedora-mac-style
+
+dnf5 install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+dnf5 install -y \
+    terra-release-extras \
+    terra-release-mesa \
+    "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+    "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+dnf5 remove -y \
+    gnome-boxes \
+    gnome-connections \
+    gnome-shell-extension-apps-menu \
+    gnome-shell-extension-background-logo \
+    gnome-shell-extension-common \
+    gnome-shell-extension-launch-new-instance \
+    gnome-shell-extension-places-menu \
+    gnome-shell-extension-window-list \
+    gnome-text-editor \
+    gnome-tour \
+    malcontent-control \
+    nano-default-editor \
+    showtime
+dnf5 upgrade --allowerasing --allow-downgrade --skip-unavailable --refresh -y
+dnf5 install --allowerasing -y \
+    alacritty \
+    antimicrox \
+    audacity-freeworld \
+    bat \
+    blender \
+    bibata-cursor-theme \
+    bottles \
+    btop \
+    btrfs-assistant \
+    bustle \
+    cargo \
+    cmatrix \
+    codium \
+    d-spy \
+    dconf-editor \
+    decibels \
+    default-fonts \
+    elisa-player \
+    eza \
+    f21-backgrounds-gnome \
+    f21-backgrounds-kde \
+    f22-backgrounds-gnome \
+    f22-backgrounds-kde \
+    f23-backgrounds-gnome \
+    f23-backgrounds-kde \
+    f24-backgrounds-gnome \
+    f24-backgrounds-kde \
+    f25-backgrounds-gnome \
+    f25-backgrounds-kde \
+    f26-backgrounds-gnome \
+    f26-backgrounds-kde \
+    f27-backgrounds-gnome \
+    f27-backgrounds-kde \
+    f28-backgrounds-gnome \
+    f28-backgrounds-kde \
+    f29-backgrounds-gnome \
+    f29-backgrounds-kde \
+    f30-backgrounds-gnome \
+    f30-backgrounds-kde \
+    f31-backgrounds-gnome \
+    f31-backgrounds-kde \
+    f32-backgrounds-gnome \
+    f32-backgrounds-kde \
+    f33-backgrounds-gnome \
+    f33-backgrounds-kde \
+    f34-backgrounds-gnome \
+    f34-backgrounds-kde \
+    f35-backgrounds-gnome \
+    f35-backgrounds-kde \
+    f36-backgrounds-gnome \
+    f36-backgrounds-kde \
+    f37-backgrounds-gnome \
+    f37-backgrounds-kde \
+    f38-backgrounds-gnome \
+    f38-backgrounds-kde \
+    f39-backgrounds-gnome \
+    f39-backgrounds-kde \
+    f40-backgrounds-gnome \
+    f40-backgrounds-kde \
+    f41-backgrounds-gnome \
+    f41-backgrounds-kde \
+    f42-backgrounds-gnome \
+    f42-backgrounds-kde \
+    f43-backgrounds-gnome \
+    f43-backgrounds-kde \
+    fastfetch \
+    ffmpeg \
+    fish \
+    freedoom \
+    freedoom2 \
+    gamescope \
+    gimp \
+    gnome-chess \
+    gnome-firmware \
+    gnome-mahjongg \
+    gnome-mines \
+    gnome-nibbles \
+    gnome-shell-extension-appindicator \
+    gnome-shell-extension-blur-my-shell \
+    gnome-shell-extension-just-perfection \
+    gnome-sudoku \
+    gnome-tweaks \
+    google-android-emoji-fonts \
+    google-arimo-fonts \
+    google-droid-fonts-all \
+    google-go-fonts \
+    google-noto-fonts-all \
+    google-noto-sans-cjk-fonts \
+    google-noto-sans-hk-fonts \
+    google-noto-serif-cjk-fonts \
+    google-roboto-fonts \
+    google-roboto-mono-fonts \
+    google-roboto-slab-fonts \
+    google-rubik-fonts \
+    gstreamer-plugins-espeak \
+    gstreamer1-plugins-bad-freeworld \
+    gstreamer1-plugins-ugly \
+    HandBrake \
+    HandBrake-gui \
+    hardinfo2 \
+    hexchat \
+    htop \
+    hydrapaper \
+    inkscape \
+    inotify-tools \
+    jetbrains-mono-fonts-all \
+    jetbrainsmono-nerd-fonts \
+    kate \
+    kdenlive \
+    kid3 \
+    kmousetool \
+    kolourpaint \
+    kpat \
+    krename \
+    krita \
+    kstars \
+    kvantum \
+    libavcodec-freeworld \
+    libcurl-devel \
+    libdnf5-plugin-actions \
+    libheif-freeworld \
+    libreoffice-base \
+    libreoffice-draw \
+    libreoffice-math \
+    libxcrypt-compat \
+    lutris \
+    material-icons-fonts \
+    mc \
+    memtest86+ \
+    mesa-vulkan-drivers.x86_64 \
+    mission-center \
+    mozilla-openh264 \
+    nano \
+    nerd-fonts \
+    obs-studio \
+    okteta \
+    openrgb \
+    openttd \
+    pipewire-codec-aptx \
+    protontricks \
+    qbittorrent \
+    qrca \
+    qt5ct \
+    qt6ct \
+    radeontop \
+    remmina \
+    rpmfusion-free-appstream-data \
+    rpmfusion-free-obsolete-packages \
+    rpmfusion-nonfree-appstream-data \
+    rpmfusion-nonfree-obsolete-packages \
+    rsms-inter-fonts \
+    rsms-inter-vf-fonts \
+    rust \
+    setroubleshoot \
+    steam \
+    snapper \
+    terminus-fonts \
+    terminus-fonts-console \
+    vim-default-editor \
+    virt-manager \
+    vlc \
+    vlc-plugins-all \
+    vlc-plugins-freeworld \
+    waycheck \
+    wine \
+    wine-alsa \
+    wine-pulseaudio \
+    winetricks \
+    yazi \
+    zed
+dnf5 autoremove -y
+dnf5 install -y nano
+systemctl disable NetworkManager-wait-online.service
+dracut --regenerate-all -f -v
+fastfetch
---
+++ fp.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+flatpak install flathub -y \
+com.adamcake.Bolt \
+com.belmoussaoui.Decoder\
+com.belmoussaoui.Obfuscate \
+com.brave.Browser \
+com.discordapp.Discord \
+com.markopejic.downloader \
+com.mattjakeman.ExtensionManager \
+com.pokemmo.PokeMMO \
+com.protonvpn.www \
+com.rafaelmardojai.Blanket \
+com.vixalien.sticky \
+com.vysp3r.ProtonPlus \
+dev.bragefuglseth.Keypunch \
+io.edcd.EDMarketConnector \
+io.gitlab.adhami3310.Converter \
+io.github.diegoivan.pdf_metadata_editor \
+io.github.fizzyizzy05.binary \
+io.github.idevecore.Valuta \
+io.github.plrigaux.sysd-manager \
+io.github.pol_rivero.github-desktop-plus \
+io.github.realmazharhussain.GdmSettings \
+io.github.Soundux \
+io.github.wartybix.Constrict \
+md.obsidian.Obsidian \
+me.proton.Mail \
+me.proton.Pass \
+net.runelite.RuneLite \
+org.indii.mendingwall \
+org.localsend.localsend_app \
+org.gnome.gitlab.YaLTeR.VideoTrimmer \
+org.signal.Signal \
+ro.go.hmlendea.DL-Desktop \
+sh.ppy.osu
